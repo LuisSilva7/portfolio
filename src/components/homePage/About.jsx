2b129668813d5e9d848214b440100b064bf70c3d
LuisSilva7 <lmpsilva7@gmail.com>
2025-01-06 19:30:44 +0000

    fix

--- src/components/homePage/About.jsx
+++ src/components/homePage/About.jsx
@@ -16,13 +16,14 @@ const About = () => {
             <h2>About Me</h2>
             <p>
               I'm a Master's student in Engineering and Management of
-              Information Systems with a passion for problem-solving and
-              building software. I specialize in backend development with Java
-              and Spring Boot, where I focus on creating secure and scalable
-              systems. While backend is my strong suit, I'm also capable of
-              handling frontend tasks to deliver end-to-end solutions. I adapt
-              quickly to new challenges and technologies, always aiming to
-              transform complex problems into practical and effective results.
+              Information Systems, living in Braga, with a passion for
+              problem-solving and building software. I specialize in backend
+              development with Java and Spring Boot, where I focus on creating
+              secure and scalable systems. While backend is my strong suit, I'm
+              also capable of handling frontend tasks to deliver end-to-end
+              solutions. I adapt quickly to new challenges and technologies,
+              always aiming to transform complex problems into practical and
+              effective results.
             </p>
           </div>
           <div className={styles.bottomPart}>
